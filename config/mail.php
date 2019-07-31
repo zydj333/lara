519c4b67c65e48be5220c7f8dd499a22cd18395e
赵洋 <286923698@qq.com>
2019-07-31 22:35:06 +0800

    配置邮箱。修改php7.2邮箱bug

--- config/mail.php
+++ config/mail.php
@@ -29,7 +29,7 @@ return [
     |
     */
 
-    'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
+    'host' => env('MAIL_HOST', 'smtp.qq.com'),
 
     /*
     |--------------------------------------------------------------------------
@@ -42,7 +42,7 @@ return [
     |
     */
 
-    'port' => env('MAIL_PORT', 587),
+    'port' => env('MAIL_PORT', 465),
 
     /*
     |--------------------------------------------------------------------------
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', '286923698@qq.com'),
+        'name' => env('MAIL_FROM_NAME', '国光等等我'),
     ],
 
     /*
@@ -71,7 +71,7 @@ return [
     |
     */
 
-    'encryption' => env('MAIL_ENCRYPTION', 'tls'),
+    'encryption' => env('MAIL_ENCRYPTION', 'ssl'),
 
     /*
     |--------------------------------------------------------------------------
@@ -88,6 +88,11 @@ return [
 
     'password' => env('MAIL_PASSWORD'),
 
+    'stream'    => [
+        'ssl' => [
+            'verify_peer' => false
+        ],
+    ],
     /*
     |--------------------------------------------------------------------------
     | Sendmail System Path
